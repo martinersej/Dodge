997b7278a300a1e9d4ea1d118212a947752b6a3c
Martin Pedersen <martinpugdal@live.dk>
2022-08-18 14:25:31 +0200

    Initial commit

---
+++ Dodgeball.java
@@ -0,0 +1,27 @@
+// Version: 20200917
+// Handin done by:
+//   <id 1> <name 1>
+//   <id 2> <name 2>
+//   <id 3> <name 3>
+// Contributions:
+//   <name 1> <contribution>
+//   <name 2> <contribution>
+//   <name 3> <contribution>
+
+import java.io.*;
+import java.util.*;
+public class Dodgeball {
+    // Add any private fields you might need here
+    public TreeSet<Integer> tree = new TreeSet<>();
+    
+    public void addPlayer(int x) {
+        tree.add(x);
+        // Implement your code here to add a player to the line
+    }
+
+    public int throwBall(int x) {
+        int distance = 42;
+        // Implement your code here to update the line of players and return the distance
+        return distance;
+    }
+}
